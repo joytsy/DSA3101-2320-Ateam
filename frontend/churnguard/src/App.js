71b0adb3a48bd245707fb4e3164849bac99e4bbb
carinacwq <carina.chiuwenqi@gmail.com>
2024-03-31 23:41:25 +0800

    Created home page and added redirection from login page to home page.

--- frontend/churnguard/src/App.js
+++ frontend/churnguard/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginSignUp from './Components/LoginSignUp/LoginSignUp';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div>
-      <LoginSignUp />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <LoginSignUp /> } />
+        <Route path="/Home" element={ <Home /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
