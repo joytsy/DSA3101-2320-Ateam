fc58187a3bc61c3547ebb56d378041dbe23ba9df
carinacwq <carina.chiuwenqi@gmail.com>
2024-04-07 23:04:14 +0800

    Edited login page.

--- frontend/churnguard/src/Components/LoginSignUp/LoginSignUp.jsx
+++ frontend/churnguard/src/Components/LoginSignUp/LoginSignUp.jsx
@@ -39,44 +39,46 @@ const LoginSignUp = () => {
     }
 
     return (
-        <div className='container'>
-            <div className="header">
-                <div className="text">{action}</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                {action === "Login" ? null : (
+        <div className="background-wrapper">
+            <div className='container'>
+                < div className="header">
+                    <div className="text">{action}</div>
+                    <div className="underline"></div> 
+                </div>
+                <div className="inputs">
+                    {action === "Login" ? null : (
+                        <div className="input">
+                            <img src={user_icon} alt="" />
+                            <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                        </div>
+                    )}
                     <div className="input">
-                        <img src={user_icon} alt="" />
-                        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                        <img src={email_icon} alt="" />
+                        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
+                    <div className="input">
+                        <img src={password_icon} alt="" />
+                        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div> 
+                </div>
+                {action === "Sign Up" ? null : (
+                    <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
                 )}
-                <div className="input">
-                    <img src={email_icon} alt="" />
-                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div className="input">
-                    <img src={password_icon} alt="" />
-                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-            </div>
-            {action === "Sign Up" ? null : (
-                <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
-            )}
-            <div className="submit-container">
-                <div
-                    className={action === "Login" ? "submit gray" : "submit"}
-                    onClick={toggleAction}
-                    disabled={!isFormFilled()}
-                    style={{ opacity: isFormFilled() ? 1 : 0.5 }}
-                >
-                    Sign Up
-                </div>
-                <div
-                    className={action === "Sign Up" ? "submit gray" : "submit"}
-                    onClick={toggleAction}
-                >
-                    Login
+                <div className="submit-container">
+                    <div
+                        className={action === "Login" ? "submit gray" : "submit"}
+                        onClick={toggleAction}
+                        disabled={!isFormFilled()}
+                        style={{ opacity: isFormFilled() ? 1 : 0.5 }}
+                    >
+                        Sign Up
+                    </div>
+                    <div
+                        className={action === "Sign Up" ? "submit gray" : "submit"}
+                        onClick={toggleAction}
+                    >
+                        Login
+                    </div>
                 </div>
             </div>
         </div>
