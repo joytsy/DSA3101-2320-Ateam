2900a0510b5cbe006c8d52f9a82ba431edd23cc2
carinacwq <carina.chiuwenqi@gmail.com>
2024-04-09 23:25:21 +0800

    Added top bar but can't see toggle icon

--- frontend/churnguard/src/Components/Navbar.jsx
+++ frontend/churnguard/src/Components/Navbar.jsx
@@ -3,21 +3,32 @@ import './Navbar.css';
 
 function Navbar() {
     const [active, setActive] = useState("nav__menu");
-  return (
+    const [toggleIcon, setToggleIcon] = useState("nav__toggler");
+    const navToggle = () => {
+      if (active === "nav__menu") {
+        setActive("nav__menu nav__active");
+      } else setActive("nav__menu");
+  
+      // Icon Toggler
+      if (toggleIcon === "nav__toggler") {
+        setToggleIcon("nav__toggler toggle");
+      } else setToggleIcon("nav__toggler");
+    };
+    return (
     <nav className="nav">
-        <a href="#" className="nav__brand">Churnguard</a>
-        <ul className={active}>
-            <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
-            <li className="nav__item"><a href="#" className="nav__link">Dashboard</a></li>
-            <li className="nav__item"><a href="#" className="nav__link">Chatbot</a></li>
-        </ul>
-        <div className="nav__toggler">
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
-        </div>
+      <a href="#" className="nav__brand">Churnguard</a>
+      <ul className={active}>
+        <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
+        <li className="nav__item"><a href="#" className="nav__link">Dashboard</a></li>
+        <li className="nav__item"><a href="#" className="nav__link">Chatbot</a></li>
+      </ul>
+      <div onClick = {navToggle} className={toggleIcon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
